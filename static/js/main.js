2a1ceee55c6b13e84336e6295445c2d7e44da94c
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-09-20 18:52:53 +0300

    Add js layout metpage and peoples

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,46 @@ const cssHrefs = {
         'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;1,100&display=swap',
         'css/header.css',
     ],
+    metcard: [
+        'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;1,100&display=swap',
+        'css/metcard.css',
+        'css/main.css',
+    ],
+    main: [
+        'css/main.css',
+    ],
+    usercard: [
+        'css/usercard.css'
+    ]
+}
+
+const appConfig = {
+    forMe: {
+        text: 'Для меня',
+        href: '/forme',
+    },
+    meetings: {
+        text: 'Мероприятия',
+        href: '/meetings',
+        open: createMetPage,
+    },
+    people: {
+        text: 'Люди',
+        href: '/peoples',
+        open: createPeoplesPage,
+    },
+    profile: {
+        text: 'Профиль',
+        href: '',
+    },
+    registration: {
+        text: "Регистрация",
+        href: "/registration",
+    },
+    login: {
+        text: "Логин",
+        href: "/login",
+    }
 }
 
 function addCSS(elemName) {
@@ -22,7 +62,19 @@ function addCSS(elemName) {
 }
 
 function createHeader() {
-    const header = document.createElement('header');
+    const tmp = document.createElement('div');
+    tmp.innerHTML = `
+        <header class="header">
+            <img src="assets/google.png" class="logo">
+            <input type="search" placeholder="Люди, мероприятия" class="searchinput">
+            <img src="assets/pericon.svg" class="icon" onclick="window.open('profile.html')">
+        </header>
+    `
+
+    addCSS('header');
+    application.appendChild(tmp.firstElementChild);
+
+    /*const header = document.createElement('header');
     header.classList.add('header');
 
     const logo = document.createElement('img');
@@ -44,39 +96,27 @@ function createHeader() {
 
     addCSS('header');
 
-    console.log(application);
-    application.appendChild(header);
+    application.appendChild(header);*/
 }
 
 function createNavigation() {
     const navigation = document.createElement('nav');
     navigation.classList.add('navigation');
 
-    const navSettings = {
-        forMe: {
-            text: 'Для меня',
-            href: '',
-            class: ['navpoint'],
-        },
-        meetings: {
-            text: 'Мероприятия',
-            href: '',
-            class: ['navpoint'],
-        },
-        people: {
-            text: 'Люди',
-            href: '',
-            class: ['navpoint'],
-        },
-    }
+    const navSettings = [
+        'forMe',
+        'meetings',
+        'people',
+    ];
 
-    Object.keys(navSettings).forEach(key => {
-        option = navSettings[key];
+    navSettings.forEach(key => {
+        let option = appConfig[key];
 
         const navPoint = document.createElement('a');
         navPoint.innerHTML = option.text;
         navPoint.href = option.href;
-        navPoint.classList = option.class;
+        navPoint.dataset.section = key;
+        navPoint.classList.add('navpoint');
 
         navigation.appendChild(navPoint);
     });
@@ -86,5 +126,179 @@ function createNavigation() {
     application.appendChild(navigation);
 }
 
-createHeader();
-createNavigation();
+function createMetCard(data) {
+    const tmp = document.createElement('div');
+    tmp.innerHTML = `
+        <div class="metcard">
+            <img src="${data.imgSrc}" class="metimg">
+            <div class="swimblock top">
+                <span>${data.text}</span>
+                <div class="tabels"></div>  
+            </div>
+            <h3>${data.title}</h3>
+            <h4>${data.place}</h4>
+            <h4>${data.date}</h4>
+        </div>
+    `;
+
+    const labels = tmp.getElementsByClassName('tabels')[0];
+    data.labels.forEach(labelText => {
+        const label = document.createElement('span');
+        label.classList.add('chips');
+        label.innerHTML = labelText;
+
+        labels.appendChild(label);
+    });
+
+    addCSS('metcard'); 
+
+    return tmp.firstElementChild;
+
+    /*const metCard = document.createElement('div');
+    metCard.classList.add('metcard');
+
+    const metImg = document.createElement('img');
+    metImg.src = data.imgSrc;
+    metImg.classList.add('metimg');
+
+    const swimBlock = document.createElement('div');
+    swimBlock.classList = ['swimblock top'];
+
+    const mainText = document.createElement('span');
+    mainText.innerHTML = data.text;
+
+    const tabels = document.createElement('div');
+    tabels.classList.add('tabels');
+
+    data.labels.forEach(labelText => {
+        const label = document.createElement('span');
+        label.classList.add('chips');
+        label.innerHTML = labelText;
+
+        tabels.appendChild(label);
+    });
+
+    swimBlock.appendChild(mainText);
+    swimBlock.appendChild(tabels);
+
+    const titleElem = document.createElement('h3');
+    titleElem.innerHTML = data.title;
+
+    const placeElem = document.createElement('h4');
+    placeElem.innerHTML = data.place;
+
+    const dateElem = document.createElement('h4');
+    dateElem.innerHTML = data.date;
+
+    metCard.appendChild(metImg);
+    metCard.appendChild(swimBlock);
+    metCard.appendChild(titleElem);
+    metCard.appendChild(placeElem);
+    metCard.appendChild(dateElem);
+
+    addCSS('metcard');
+
+    return metCard;*/
+}
+
+
+function createUserCard(data) {
+    const tmp = document.createElement('div');
+    tmp.innerHTML = `
+        <div class="usercard">
+            <img src="${data.imgSrc}" class="overlay">
+            <div class="overlay"></div>
+            <div class="wraper top">
+                <h2>${data.name}</h2>
+                <span>${data.job}</span>
+                
+                <h3>Интересы</h3>
+                <div class="tabels"></div>
+                
+                <h3>Навыки</h3>
+                <div class="tabels"></div>
+            </div>
+        </div>`;
+
+    const interestings = tmp.getElementsByClassName('tabels')[0];
+    data.interestings.forEach(labelText => {
+        const label = document.createElement('span');
+        label.classList.add('chips');
+        label.innerHTML = labelText;
+
+        interestings.appendChild(label);
+    });
+
+    const skills = tmp.getElementsByClassName('tabels')[1];
+    data.skills.forEach(labelText => {
+        const label = document.createElement('span');
+        label.classList.add('chips');
+        label.innerHTML = labelText;
+
+        skills.appendChild(label);
+    });
+
+    addCSS('usercard');
+    
+    return tmp.firstElementChild;
+}
+
+function createMetPage() {
+    application.innerHTML = '';
+    createHeader();
+    createNavigation();
+
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    for (let i = 0; i < 10; i++) {
+        main.appendChild(createMetCard({
+            text: `Lorem ipsum dolor sit amet,
+                   consectetur adipiscing elit, sed
+                   do eiusmod tempor incididunt ut
+                   labore et dolore magna aliqua.
+                   Ut enim ad minim veniam, quis
+                   nostrud exercitation ullamco labori`,
+            imgSrc: 'assets/paris.jpg',
+            labels: ['Rust', 'Забив', 'В падике'],
+            title: 'Забив с++',
+            place: 'Дом Пушкина, улица Калатушкина',
+            date: '12 сентября 2020',
+        }));
+    }
+
+    application.appendChild(main);
+}
+
+function createPeoplesPage() {
+    application.innerHTML = '';
+    createHeader();
+    createNavigation();
+
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    for (let i = 0; i < 10; i++) {
+        main.appendChild(createUserCard({
+            imgSrc: 'assets/luckash.jpeg',
+            name: 'Александр',
+            job: 'Главный чекист КГБ',
+            interestings: ['Картофель', 'Хоккей'],
+            skills: ['Разгон митингов', 'Сбор урожая'],
+        }));
+    }
+
+    application.appendChild(main);
+}
+
+
+createMetPage();
+
+application.addEventListener('click', (evt) => {
+    const {target} = evt;
+ 
+    if (target instanceof HTMLAnchorElement) {
+        evt.preventDefault();
+        appConfig[target.dataset.section].open();
+    }
+ });
